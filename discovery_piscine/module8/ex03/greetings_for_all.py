7aa310b76d7cd2b5f1adbcd8bcc67a4c1d49aeba
Svetlana Panfilova <storba@gmail.com>
2025-07-02 20:32:40 +0200

    use isinstance

--- discovery_piscine/module8/ex03/greetings_for_all.py
+++ discovery_piscine/module8/ex03/greetings_for_all.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def greetings(name="noble stranger"):
-    if type(name) is str:
+    #if type(name) is str:
+    if isinstance(name, str):
             print(f"Hello, {name}")
     else:
         print("Error! It was not a name.")
